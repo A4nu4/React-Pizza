cf40a1013f3448bff09b6068969f12eb352327e6
Arnur Omirserikov <arnuromirserikov@Arnurs-MacBook-Air.local>
2026-08-12 12:19:14 +0500

    Added logo and count button on Pizza Block and switching categories

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,13 +1,51 @@
+import React from "react";
+
 export default function Categories() {
+  const [activeIndex, setActiveIndex] = React.useState(0);
+
+  const onClickCategory = (id) => {
+    setActiveIndex(id);
+  };
+
   return (
-    <div class="categories">
+    <div className="categories">
       <ul>
-        <li class="active">Все</li>
-        <li>Мясные</li>
-        <li>Вегетарианская</li>
-        <li>Гриль</li>
-        <li>Острые</li>
-        <li>Закрытые</li>
+        <li
+          onClick={() => onClickCategory(0)}
+          className={activeIndex === 0 ? "active" : ""}
+        >
+          Все
+        </li>
+        <li
+          onClick={() => onClickCategory(1)}
+          className={activeIndex === 1 ? "active" : ""}
+        >
+          Мясные
+        </li>
+        <li
+          onClick={() => onClickCategory(2)}
+          className={activeIndex === 2 ? "active" : ""}
+        >
+          Вегетарианская
+        </li>
+        <li
+          onClick={() => onClickCategory(3)}
+          className={activeIndex === 3 ? "active" : ""}
+        >
+          Гриль
+        </li>
+        <li
+          conClick={() => onClickCategory(4)}
+          lassName={activeIndex === 4 ? "active" : ""}
+        >
+          Острые
+        </li>
+        <li
+          onClick={() => onClickCategory(5)}
+          className={activeIndex === 5 ? "active" : ""}
+        >
+          Закрытые
+        </li>
       </ul>
     </div>
   );
