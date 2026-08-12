9c2107d9874469ae5f3637edd2891b9568228848
Arnur Omirserikov <arnuromirserikov@Arnurs-MacBook-Air.local>
2026-08-12 16:51:20 +0500

    Added backend request

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Header from "./components/Header.jsx";
 import Categories from "./components/Categories.jsx";
@@ -9,13 +9,25 @@ import pizzas from "./assets/img/pizzas.json";
 import "./scss/app.scss";
 
 function App() {
+  const [items, setItems] = React.useState([]);
+
+  React.useEffect(() => {
+    fetch("https://6a7c56b1a008c10e4cbf4595.mockapi.io/items")
+      .then((res) => {
+        return res.json();
+      })
+      .then((arr) => {
+        setItems(arr);
+      });
+  }, []);
+
   return (
     <div className="wrapper">
       <Header />
       <div className="content">
         <div className="container">
           <div className="content__top">
-            {/*  Categories */}
+            <Categories />
             <Sort />
           </div>
           <h2 className="content__title">Все пиццы</h2>
