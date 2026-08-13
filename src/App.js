72df6723db9a6499b0d233ea21074045095a19f6
Arnur Omirserikov <arnuromirserikov@Arnurs-MacBook-Air.local>
2026-08-13 10:51:55 +0500

    Added Cart, Home, NotFound pages and implemented react routing

--- src/App.js
+++ src/App.js
@@ -1,44 +1,25 @@
-import React, { useState } from "react";
+import React from "react";
+import { Routes, Route } from "react-router-dom";
 
 import Header from "./components/Header.jsx";
-import Categories from "./components/Categories.jsx";
-import Sort from "./components/Sort.jsx";
-import PizzaBlock from "./components/PizzaBlock";
-import Skeleton from "./components/PizzaBlock/Skeleton.jsx";
 
-import pizzas from "./assets/img/pizzas.json";
+import Home from "./pages/Home.jsx";
+import Cart from "./pages/Cart.jsx";
+import NotFound from "./pages/NotFound.jsx";
+
 import "./scss/app.scss";
 
 function App() {
-  const [items, setItems] = React.useState([]);
-  const [isLoading, setIsLoading] = React.useState(true);
-
-  React.useEffect(() => {
-    fetch("https://6a7c56b1a008c10e4cbf4595.mockapi.io/items")
-      .then((res) => {
-        return res.json();
-      })
-      .then((arr) => {
-        setItems(arr);
-        setIsLoading(false);
-      });
-  }, []);
-
   return (
     <div className="wrapper">
       <Header />
       <div className="content">
         <div className="container">
-          <div className="content__top">
-            <Categories />
-            <Sort />
-          </div>
-          <h2 className="content__title">Все пиццы</h2>
-          <div className="content__items">
-            {isLoading
-              ? [...new Array(6)].map(() => <Skeleton />)
-              : items.map((obj) => <PizzaBlock key={obj.id} {...obj} />)}
-          </div>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
         </div>
       </div>
     </div>
