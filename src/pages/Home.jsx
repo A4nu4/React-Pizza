556c1232b827b88459d46e7fee79ae1ff743f0e4
Arnur Omirserikov <arnuromirserikov@Arnurs-MacBook-Air.local>
2026-08-13 14:42:05 +0500

    Implemented sorting

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,13 +9,20 @@ const Home = () => {
   const [items, setItems] = React.useState([]);
   const [isLoading, setIsLoading] = React.useState(true);
   const [categoryId, setCategoryId] = React.useState(0);
-  const [sortType, setSortType] = React.useState(0);
+  const [sortType, setSortType] = React.useState({
+    name: "популярности",
+    sortProperty: "rating",
+  });
 
   React.useEffect(() => {
     setIsLoading(true);
+
+    const order = sortType.sortProperty.includes("-") ? "asc" : "desc";
+    const sortBy = sortType.sortProperty.replace("-", "");
+    const category = categoryId > 0 ? `category=${categoryId}` : "";
+
     fetch(
-      "https://6a7c56b1a008c10e4cbf4595.mockapi.io/items?category=" +
-        categoryId,
+      `https://6a7c56b1a008c10e4cbf4595.mockapi.io/items?${category}&sortBy=${sortBy}&order=${order}`,
     )
       .then((res) => res.json())
       .then((arr) => {
@@ -23,16 +30,16 @@ const Home = () => {
         setIsLoading(false);
       });
     window.scrollTo(0, 0);
-  }, [categoryId]);
+  }, [categoryId, sortType]);
   console.log(isLoading);
   return (
     <div className="container">
       <div className="content__top">
         <Categories
           value={categoryId}
-          onClickCategory={(i) => setCategoryId(i)}
+          onChangeCategory={(i) => setCategoryId(i)}
         />
-        <Sort />
+        <Sort value={sortType} onChangeSort={(i) => setSortType(i)} />
       </div>
       <h2 className="content__title">Все пиццы</h2>
       <div className="content__items">
