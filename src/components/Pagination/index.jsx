e2be476af9178051ba781de2c2bd7fd464f4ad66
Arnur Omirserikov <arnuromirserikov@Arnurs-MacBook-Air.local>
2026-08-14 16:14:22 +0500

    Search started interacting with server, implemented debounce and improved pagination

--- src/components/Pagination/index.jsx
+++ src/components/Pagination/index.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactPaginate from "react-paginate";
 import styles from "./Pagination.module.scss";
 
-const Pagination = ({ onChangePage }) => {
+const Pagination = ({ currentPage, onChangePage }) => {
   return (
     <ReactPaginate
       className={styles.root}
@@ -10,8 +10,9 @@ const Pagination = ({ onChangePage }) => {
       nextLabel=">"
       previousLabel="<"
       onPageChange={(event) => onChangePage(event.selected + 1)}
-      pageRangeDisplayed={5}
+      pageRangeDisplayed={4}
       pageCount={3}
+      forcePage={currentPage - 1}
       renderOnZeroPageCount={null}
     />
   );
